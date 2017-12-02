4c01255e636e9dd9629c733030a5be6a9845bf8a
KatoMono Enkeli <k4t0mono@gmail.com>
2017-12-02 10:55:52 -0200

    Lession 09: Using require

---
+++ section03/app.js
@@ -0,0 +1,16 @@
+// notes-node
+
+console.log("Starting app.");
+
+const fs = require("fs");
+const os = require("os");
+
+var user = os.userInfo();
+
+
+fs.appendFile("greetings.txt", `Hello ${user.username}!\n`, function(err) {
+	if(err) {
+		console.log("Unable to write to file");
+	}
+});
+
