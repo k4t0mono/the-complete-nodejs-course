60ca41cbb2c606ca251406dadab7a1998f63b5c8
KatoMono Enkeli <k4t0mono@gmail.com>
2017-12-02 19:27:43 -0200

    Lession 11: Using 3rd party module module

--- section03/app.js
+++ section03/app.js
@@ -4,11 +4,17 @@ console.log("Starting app.js");
 
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash");
 const notes = require("./notes.js");
 
-var res = notes.addNote();
-console.log(res);
-console.log('Result: ', notes.add(2,3));
+console.log(_.isString(true));
+console.log(_.isString("KatoMono"));
+var arr = _.uniq([1, 2, 1, 4, 5]);
+console.log(arr);
+
+//var res = notes.addNote();
+//console.log(res);
+//console.log('Result: ', notes.add(2,3));
 
 //var user = os.userInfo();
 //fs.appendFile("greetings.txt", `Hello ${user.username}! Im ${notes.age}\n`, function(err) {
