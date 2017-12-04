7cddb5cd537e51c2a793b53562914c1a1b9da8ee
KatoMono Enkeli <k4t0mono@gmail.com>
2017-12-04 00:09:17 -0200

    Lession 13: Getting input from user

--- section03/app.js
+++ section03/app.js
@@ -7,4 +7,18 @@ const _ = require("lodash");
 
 const notes = require("./notes.js");
 
+console.log(process.argv)
+var command = process.argv[2];
 
+console.log('Comand: ', command);
+if(command === 'add') {
+	console.log('Adding new note');
+} else if(command === 'list') {
+	console.log('Listing all notes');
+} else if(command === 'read') {
+	conolse.log('Reading note');
+} else if(command === 'remove') {
+	console.log('Removing nore');
+} else {
+	console.log('Command unknow');
+}
