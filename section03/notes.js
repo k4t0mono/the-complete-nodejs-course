0d8e92112356fa344043b6e6e401195fc9579b77
KatoMono Enkeli <k4t0mono@gmail.com>
2017-12-06 20:20:59 -0200

    Lession 16: Adding and saving notes

--- section03/notes.js
+++ section03/notes.js
@@ -1,9 +1,29 @@
 // notes-node
 
+const fs = require('fs');
+
 console.log("Starting notes.js");
 
 var addNote = (title, body) => {
-	console.log('Adding note:', title, body);
+	var notes = [];
+
+	try {
+		notes = JSON.parse(fs.readFileSync('notes-data.json'));
+	} catch(e) {
+		//fs.writeFileSync('notes-data.json', '');
+	}
+
+	var note = {
+		title,
+		body
+	};
+
+	var duplicateNotes = notes.filter((note) => { return note.title === title });
+	if(duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', (JSON.stringify(notes) + "\n"));
+	}
+
 };
 
 var getAll = () => {
