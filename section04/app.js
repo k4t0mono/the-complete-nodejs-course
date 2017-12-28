fff6b465edce0df6310b1180da9fc687e2804013
KatoMono Enkeli <k4t0mono@gmail.com>
2017-12-28 00:47:17 -0200

    Lession 031: Encoding user input

--- section04/app.js
+++ section04/app.js
@@ -1,9 +1,25 @@
 // app.js
 
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		address: {
+			demand: true,
+			alias: 'a',
+			describe: 'Addres to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+console.log(argv.address);
 
 request({
-		url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombad%20filadelfia',
+		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
 		json: true
 	},
 
