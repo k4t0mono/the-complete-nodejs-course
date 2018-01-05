46b022bbaf5aad6757c611ebe66cee4e59dc3f70
KatoMono Enkeli <k4t0mono@gmail.com>
2018-01-04 22:15:21 -0200

    Lession 033: Abstracting callbacks

--- section04/app.js
+++ section04/app.js
@@ -1,7 +1,8 @@
 // app.js
 
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 	.options({
@@ -16,27 +17,11 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-console.log(argv.address);
-
-request({
-		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
-		json: true
-	},
-
-	(error, response, body) => {
-		if(error) {
-			console.log('Unable to connect to Google servers.');
-
-		} else if(body.status === 'ZERO_RESULTS') {
-			console.log('Unable to find address.');
-
-		} else if(body.status === 'OK') {
-			var result = body.results[0];
-			console.log(`Address: ${result.formatted_address}`);
-			console.log(`Lat: ${result.geometry.location.lat} Lng: ${result.geometry.location.lng}`);
-
-		} else {
-			console.log('Heeeeeelp me, pleeeese!!! ;w;');
-		}
+geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+	if(errorMsg) {
+		console.log(errorMsg);
+	} else {
+		console.log(JSON.stringify(results, undefined, 2));
 	}
-);
+});
+
---
+++ section04/geocode/geocode.js
@@ -0,0 +1,36 @@
+// geocode.js
+
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+	address = encodeURIComponent(address)
+
+	request({
+		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+		json: true
+	},
+
+	(error, response, body) => {
+		if(error) {
+			callback('Unable to connect to Google servers.');
+
+		} else if(body.status === 'ZERO_RESULTS') {
+			callback('Unable to find address.');
+
+		} else if(body.status === 'OK') {
+			var result = body.results[0];
+			callback(undefined, {
+				address: result.formatted_address,
+				latitude: result.geometry.location.lat,
+				longitude: result.geometry.location.lng
+			});
+
+		} else {
+			console.log('Heeeeeelp me, pleeeese!!! ;w;');
+		}
+	});
+}
+
+module.exports = {
+	geocodeAddress
+}
