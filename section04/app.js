c9604a001d3846265b6bf6b637418221e0935916
KatoMono Enkeli <k4t0mono@gmail.com>
2018-01-04 22:59:51 -0200

    Lession 034: Wiring up weather search

--- section04/app.js
+++ section04/app.js
@@ -1,6 +1,7 @@
 // app.js
 
 const yargs = require('yargs');
+const request = require('request');
 
 const geocode = require('./geocode/geocode');
 
@@ -17,11 +18,26 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-geocode.geocodeAddress(argv.address, (errorMsg, results) => {
-	if(errorMsg) {
-		console.log(errorMsg);
-	} else {
-		console.log(JSON.stringify(results, undefined, 2));
+request({
+	url: 'https://api.darksky.net/forecast/636e7965c828c7815aa160dfd2572a10/-20.8904,-45.280?units=si',
+	json: true
+	},
+	
+	(error, response, body) => {
+		if(!error && response.statusCode === 200) {
+			console.log(`${body.currently.temperature} °C`);
+
+		} else {
+			console.log('Unable to connect to Forecasr.io server.');
+		}
 	}
-});
+);
+
+//geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+	//if(errorMsg) {
+		//console.log(errorMsg);
+	//} else {
+		//console.log(JSON.stringify(results, undefined, 2));
+	//}
+//});
 
