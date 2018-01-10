4c8274db9cacbea6f9f03bdb9fb0cabcde38b6c4
KatoMono Enkeli <k4t0mono@gmail.com>
2018-01-10 21:50:36 -0200

    Lession 043: Rendering templates with data

--- section05/server.js
+++ section05/server.js
@@ -1,25 +1,35 @@
 // sever.js
 
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	//res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'KatoMono Enkeli',
-		likes: [
-			'Mathues',
-			'Programming',
-			'Star Wars'
-		]
+	//res.send({
+		//name: 'KatoMono Enkeli',
+		//likes: [
+			//'Mathues',
+			//'Programming',
+			//'Star Wars'
+		//]
+	//});
+	res.render('home.hbs', {
+		pageTitle: 'Home page',
+		currentYear: new Date().getFullYear(),
+		welcomeMsg: 'Welcome and be assimilated ...'
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('About page');
+	res.render('about.hbs', {
+		pageTitle: 'About page',
+		currentYear: new Date().getFullYear()
+	});
 });
 
 app.get('/bad', (req, res) => {
