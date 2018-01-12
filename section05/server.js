d408150543f038f41afac3e2623e80173361e91d
KatoMono Enkeli <k4t0mono@gmail.com>
2018-01-12 20:11:45 -0200

    Lession 044: Advanced templating

--- section05/server.js
+++ section05/server.js
@@ -5,22 +5,20 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	//res.send({
-		//name: 'KatoMono Enkeli',
-		//likes: [
-			//'Mathues',
-			//'Programming',
-			//'Star Wars'
-		//]
-	//});
 	res.render('home.hbs', {
 		pageTitle: 'Home page',
-		currentYear: new Date().getFullYear(),
 		welcomeMsg: 'Welcome and be assimilated ...'
 	});
 });
@@ -28,7 +26,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
 	res.render('about.hbs', {
 		pageTitle: 'About page',
-		currentYear: new Date().getFullYear()
 	});
 });
 
