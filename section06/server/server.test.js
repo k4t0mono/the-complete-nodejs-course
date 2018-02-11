da47c1919d4bd01c69f59efa0737e29c6a825d5b
KatoMono Enkeli <k4t0mono@gmail.com>
2018-02-11 17:43:25 -0200

    Lession 057: Organizing test with describe()

--- section06/server/server.test.js
+++ section06/server/server.test.js
@@ -5,27 +5,39 @@ const expect = require('expect');
 
 var app = require('./server.js').app;
 
-it('should return hello world response', (done) => {
-	request(app)
-		.get('/')
-		.expect(404)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				error: 'Page not found'
-			});
-		})
-		.end(done);
-});
+describe('Sever', () => {
+	
+	describe('GET /', () => {
 
-it('should include KatoMono in users', (done) => {
-	request(app)
-		.get('/users')
-		.expect(200)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				name: 'KatoMono Enkeli',
-				age: 19
-			})
-		})
-		.end(done);
+		it('should return hello world response', (done) => {
+			request(app)
+				.get('/')
+				.expect(404)
+				.expect((res) => {
+					expect(res.body).toInclude({
+						error: 'Page not found'
+					});
+				})
+				.end(done);
+		});
+
+	});
+	
+	describe('GET /users', () => {
+
+		it('should include KatoMono in users', (done) => {
+			request(app)
+				.get('/users')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).toInclude({
+						name: 'KatoMono Enkeli',
+						age: 19
+					})
+				})
+				.end(done);
+		});
+
+	});
+
 });
--- section06/utils/utils.test.js
+++ section06/utils/utils.test.js
@@ -5,46 +5,53 @@ const expect = require('expect');
 const utils = require('./utils');
 
 
-it('should add two numbers', () => {
-	var res = utils.add(33, 11);
+describe('Utils', () => {
 	
-	expect(res).toBe(44).toBeA('number');
-});
+	describe('#add', () => {
 
-
-it('should return squared number', () => {
-	var res = utils.square(3);
-
-	expect(res).toBe(9).toBeA('number');
-});
-
-
-it('should verify first and last name are set', () => {
-	var k4t0 = {
-		age: 19,
-		spice: "ocelot"
-	}
-
-	var result = utils.setName(k4t0, "KatoMono Enkeli");
-
-	expect(result).toInclude({
-		firstName: "KatoMono",
-		lastName: "Enkeli"
-	}).toBeA('object');
-});
-
-
-it('should assync add two numbers', (done) => {
-	utils.assyncAdd(4, 3, (sum) => {
-		expect(sum).toBe(7).toBeA('number');
-		done();
+		it('should add two numbers', () => {
+			var res = utils.add(33, 11);
+			
+			expect(res).toBe(44).toBeA('number');
+		});
+	
 	});
-});
 
+	it('should return squared number', () => {
+		var res = utils.square(3);
 
-it('should assync square a number', (done) => {
-	utils.assyncSquare(3,  (sq) => {
-		expect(sq).toBeA('number').toBe(9);
-		done();
+		expect(res).toBe(9).toBeA('number');
 	});
+
+
+	it('should verify first and last name are set', () => {
+		var k4t0 = {
+			age: 19,
+			spice: "ocelot"
+		}
+
+		var result = utils.setName(k4t0, "KatoMono Enkeli");
+
+		expect(result).toInclude({
+			firstName: "KatoMono",
+			lastName: "Enkeli"
+		}).toBeA('object');
+	});
+
+
+	it('should assync add two numbers', (done) => {
+		utils.assyncAdd(4, 3, (sum) => {
+			expect(sum).toBe(7).toBeA('number');
+			done();
+		});
+	});
+
+
+	it('should assync square a number', (done) => {
+		utils.assyncSquare(3,  (sq) => {
+			expect(sq).toBeA('number').toBe(9);
+			done();
+		});
+	});
+
 });
