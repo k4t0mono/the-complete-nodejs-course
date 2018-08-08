9d8cf91acadb6f6b2112e847560e0f6393900ade
KatoMono Enkeli <k4t0mono@gmail.com>
2018-08-08 00:29:51 -0300

    ???

--- section10/currency_converter.js
+++ section10/currency_converter.js
@@ -1,28 +1,35 @@
-// F(USD, BRL, 20) = 75.05
-
-//http://data.fixer.io/api/latest?access_key=22a451464e94823e7f1f238235c5fed1&format=1
-
 const axios = require('axios');
 
 const FIXER_KEY = process.env['FIXER_KEY']
-const BASE_URL = 'http://data.fixer.io/api/latest'
-
 
 const get_extenge_rate = async (from, to) => {
-	const url = `${BASE_URL}?access_key=${FIXER_KEY}&format=1`;
-	const response = await  axios.get(url);
+	try {
+		const url = `http://data.fixer.io/api/latest?access_key=${FIXER_KEY}&format=1`;
+		const response = await axios.get(url);
+		if(!response.data.success) {
+			throw new Error(`Error on 'fixer.io': ${response.data.error.type}`);
+		}
 
-	const euro = 1 / response.data.rates[from];
-	const rate = euro * response.data.rates[to];
+		const euro = 1 / response.data.rates[from];
+		const rate = euro * response.data.rates[to];
 
-	return rate;
+		return rate;
+
+	} catch(e) {
+		throw new Error(`Unable to fetch data from 'fixer.io'\n${e.message}`);
+	}
 };
 
 const get_countries = async (currency) => {
-	const url = `https://restcountries.eu/rest/v2/currency/${currency}`;
-	const response = await axios.get(url);
+	try {
+		const url = `https://restcountries.eu/rest/v2/currency/${currency}`;
+		const response = await axios.get(url);
 
-	return response.data.map((c) => c.name);
+		return response.data.map((c) => c.name);
+
+	} catch(e) {
+		throw new Error(`Unable to fetch data from 'restcountriess.eu'\n${e.message}`);
+	}
 };
 
 const convert_currency = async (from, to, amount) => {
@@ -39,4 +46,4 @@ const convert_currency = async (from, to, amount) => {
 
 convert_currency('ARS', 'BRL', 500)
 	.then((r) => console.log(r))
-	.catch((e) => console.log(e));
+	.catch((e) => console.log(e.message));
